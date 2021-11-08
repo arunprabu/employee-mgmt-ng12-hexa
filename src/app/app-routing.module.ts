91c056433fe89bccb79bcdfa6950f8f88390c34b
Arun Vijayarengan <arunprabu.v@hotmail.com>
2021-11-08 21:04:51 +0530

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AboutComponent } from './about/about.component';
 import { ConceptsComponent } from './concepts/components/concepts.component';
-import { EmployeesComponent } from './employees/employees.component';
+import { AddEmployeeComponent } from './employees/components/add-employee/add-employee.component';
+import { EmployeeDetailsComponent } from './employees/components/employee-details/employee-details.component';
+import { EmployeesComponent } from './employees/components/employees.component';
 import { HomeComponent } from './home/home.component';
 
 // Configuring the Routes/URLs 
@@ -11,6 +13,8 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'concepts', component: ConceptsComponent },
   { path: 'employees', component: EmployeesComponent },
+  { path: 'employees/add', component: AddEmployeeComponent },
+  { path: 'employees/1', component: EmployeeDetailsComponent },
   { path: 'about', component: AboutComponent },
 ];
 
